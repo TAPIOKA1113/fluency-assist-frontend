8230822e62f77486a8490194f3a616731803fec5
TAPIOKA1113 <manato20021113@gmail.com>
2024-10-21 16:44:19 +0900

    選択した文字をハイライトする処理の実装

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,27 +1,61 @@
-import { EditorState } from "draft-js";
-import React, { useState } from "react";
+import { EditorState, convertToRaw, RichUtils } from "draft-js";
+import { useState, useEffect } from "react";
 import { Editor } from "react-draft-wysiwyg";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 const TextEditor = () => {
-
     const [editorState, setEditorState] = useState(() =>
         EditorState.createEmpty()
     );
 
+    const handleEditorStateChange = (newEditorState: EditorState) => {
+        setEditorState(newEditorState);
+    };
+
+    // カスタムスタイルマップを定義
+    const customStyleMap = {
+        'HIGHLIGHT': {
+            background: 'yellow',
+        },
+    };
+
+    // ハイライトを適用する関数
+    const applyHighlight = () => {
+        const newEditorState = RichUtils.toggleInlineStyle(
+            editorState,
+            'HIGHLIGHT'
+        );
+        setEditorState(newEditorState);
+
+    };
+
+    useEffect(() => {
+        const contentState = editorState.getCurrentContent();
+        const rawContentState = convertToRaw(contentState);
+        console.log('エディターの内容:', JSON.stringify(rawContentState, null, 2));
+    }, [editorState]);
+
+
+
+
     return (
-        <div className="editor">
-            <Editor
-                editorState={editorState}
-                toolbarClassName="toolbarClassName"
-                wrapperClassName="wrapperClassName"
-                editorClassName="editorClassName"
-                onEditorStateChange={setEditorState}
-                localization={{
-                    locale: "ja",
-                }}
-            />
-        </div>
+        <>
+            <div className="editor">
+                <Editor
+                    editorState={editorState}
+                    toolbarClassName="toolbarClassName"
+                    wrapperClassName="wrapperClassName"
+                    editorClassName="editorClassName"
+                    onEditorStateChange={handleEditorStateChange}
+                    customStyleMap={customStyleMap}
+                    localization={{
+                        locale: "ja",
+                    }}
+                />
+                <button onClick={applyHighlight}>ハイライト</button>
+            </div>
+
+
+        </>
     );
 };
 
